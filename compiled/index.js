f72ff12c74f466b44166e34850c909854b3c5bb0
David Sargeant <chorpler@gmail.com>
2018-12-28 16:03:24 -0600

    Changed the way index.ts works

--- compiled/index.js
+++ compiled/index.js
@@ -1,50 +1,11 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var admins_1 = require("./admins");
-var authentication_1 = require("./authentication");
-var users_1 = require("./users");
-var plugin = {};
-plugin.login = authentication_1.logIn;
-plugin.logIn = authentication_1.logIn;
-plugin.logout = authentication_1.logOut;
-plugin.logOut = authentication_1.logOut;
-plugin.getSession = authentication_1.getSession;
-plugin.getMembership = admins_1.getMembership;
-plugin.signUpAdmin = admins_1.signUpAdmin;
-plugin.deleteAdmin = admins_1.deleteAdmin;
-plugin.getUsersDatabaseUrl = users_1.getUsersDatabaseUrl;
-plugin.signup = users_1.signUp;
-plugin.signUp = users_1.signUp;
-plugin.getUser = users_1.getUser;
-plugin.putUser = users_1.putUser;
-plugin.deleteUser = users_1.deleteUser;
-plugin.changePassword = users_1.changePassword;
-plugin.changeUsername = users_1.changeUsername;
-// export class PouchDBPlugin {
-//   public login = logIn;
-//   public logIn = logIn;
-//   public logout = logOut;
-//   public logOut = logOut;
-//   public getSession = getSession;
-//   public getMembership = getMembership;
-//   public signUpAdmin = signUpAdmin;
-//   public deleteAdmin = deleteAdmin;
-//   public getUsersDatabaseUrl = getUsersDatabaseUrl;
-//   public signup = signUp;
-//   public signUp = signUp;
-//   public getUser = getUser;
-//   public putUser = putUser;
-//   public deleteUser = deleteUser;
-//   public changePassword = changePassword;
-//   public changeUsername = changeUsername;
-//   constructor() {
-//   }
-// }
-// // let plugin:any = PouchDBPlugin;
-// let plugin:PouchDBPlugin = new PouchDBPlugin();
-if (typeof window !== 'undefined' && window.PouchDB) {
-    window.PouchDB.plugin(plugin);
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-exports.default = plugin;
-// export { plugin };
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./admins"));
+__export(require("./authentication"));
+__export(require("./plugin"));
+__export(require("./users"));
+__export(require("./utils"));
 //# sourceMappingURL=index.js.map
---
+++ src/plugin.ts
@@ -0,0 +1,64 @@
+declare const window:any;
+
+import { deleteAdmin, getMembership, signUpAdmin } from "./admins";
+import { getSession, logIn, logOut } from "./authentication";
+import {
+  changePassword,
+  changeUsername,
+  deleteUser,
+  getUser,
+  getUsersDatabaseUrl,
+  putUser,
+  signUp,
+} from "./users";
+
+let plugin:any = {};
+plugin.login = logIn;
+plugin.logIn = logIn;
+plugin.logout = logOut;
+plugin.logOut = logOut;
+plugin.getSession = getSession;
+
+plugin.getMembership = getMembership;
+plugin.signUpAdmin = signUpAdmin;
+plugin.deleteAdmin = deleteAdmin;
+
+plugin.getUsersDatabaseUrl = getUsersDatabaseUrl;
+plugin.signup = signUp;
+plugin.signUp = signUp;
+plugin.getUser = getUser;
+plugin.putUser = putUser;
+plugin.deleteUser = deleteUser;
+plugin.changePassword = changePassword;
+plugin.changeUsername = changeUsername;
+// export class PouchDBPlugin {
+//   public login = logIn;
+//   public logIn = logIn;
+//   public logout = logOut;
+//   public logOut = logOut;
+//   public getSession = getSession;
+//   public getMembership = getMembership;
+//   public signUpAdmin = signUpAdmin;
+//   public deleteAdmin = deleteAdmin;
+//   public getUsersDatabaseUrl = getUsersDatabaseUrl;
+//   public signup = signUp;
+//   public signUp = signUp;
+//   public getUser = getUser;
+//   public putUser = putUser;
+//   public deleteUser = deleteUser;
+//   public changePassword = changePassword;
+//   public changeUsername = changeUsername;
+//   constructor() {
+
+//   }
+// }
+
+// // let plugin:any = PouchDBPlugin;
+// let plugin:PouchDBPlugin = new PouchDBPlugin();
+
+if(typeof window !== 'undefined' && window.PouchDB) {
+  window.PouchDB.plugin(plugin);
+}
+
+// export default plugin;
+export { plugin };
