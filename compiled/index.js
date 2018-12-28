a47f3b8155485df220241f46a590a439244c7679
David Sargeant <chorpler@gmail.com>
2018-12-28 15:55:11 -0600

    Export changed again

--- compiled/index.js
+++ compiled/index.js
@@ -3,47 +3,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var admins_1 = require("./admins");
 var authentication_1 = require("./authentication");
 var users_1 = require("./users");
-// let plugin:any = {};
-// plugin.login = logIn;
-// plugin.logIn = logIn;
-// plugin.logout = logOut;
-// plugin.logOut = logOut;
-// plugin.getSession = getSession;
-// plugin.getMembership = getMembership;
-// plugin.signUpAdmin = signUpAdmin;
-// plugin.deleteAdmin = deleteAdmin;
-// plugin.getUsersDatabaseUrl = getUsersDatabaseUrl;
-// plugin.signup = signUp;
-// plugin.signUp = signUp;
-// plugin.getUser = getUser;
-// plugin.putUser = putUser;
-// plugin.deleteUser = deleteUser;
-// plugin.changePassword = changePassword;
-// plugin.changeUsername = changeUsername;
-var PouchDBPlugin = /** @class */ (function () {
-    function PouchDBPlugin() {
-        this.login = authentication_1.logIn;
-        this.logIn = authentication_1.logIn;
-        this.logout = authentication_1.logOut;
-        this.logOut = authentication_1.logOut;
-        this.getSession = authentication_1.getSession;
-        this.getMembership = admins_1.getMembership;
-        this.signUpAdmin = admins_1.signUpAdmin;
-        this.deleteAdmin = admins_1.deleteAdmin;
-        this.getUsersDatabaseUrl = users_1.getUsersDatabaseUrl;
-        this.signup = users_1.signUp;
-        this.signUp = users_1.signUp;
-        this.getUser = users_1.getUser;
-        this.putUser = users_1.putUser;
-        this.deleteUser = users_1.deleteUser;
-        this.changePassword = users_1.changePassword;
-        this.changeUsername = users_1.changeUsername;
-    }
-    return PouchDBPlugin;
-}());
-exports.PouchDBPlugin = PouchDBPlugin;
-// let plugin:any = PouchDBPlugin;
-var plugin = new PouchDBPlugin();
+var plugin = {};
+plugin.login = authentication_1.logIn;
+plugin.logIn = authentication_1.logIn;
+plugin.logout = authentication_1.logOut;
+plugin.logOut = authentication_1.logOut;
+plugin.getSession = authentication_1.getSession;
+plugin.getMembership = admins_1.getMembership;
+plugin.signUpAdmin = admins_1.signUpAdmin;
+plugin.deleteAdmin = admins_1.deleteAdmin;
+plugin.getUsersDatabaseUrl = users_1.getUsersDatabaseUrl;
+plugin.signup = users_1.signUp;
+plugin.signUp = users_1.signUp;
+plugin.getUser = users_1.getUser;
+plugin.putUser = users_1.putUser;
+plugin.deleteUser = users_1.deleteUser;
+plugin.changePassword = users_1.changePassword;
+plugin.changeUsername = users_1.changeUsername;
+// export class PouchDBPlugin {
+//   public login = logIn;
+//   public logIn = logIn;
+//   public logout = logOut;
+//   public logOut = logOut;
+//   public getSession = getSession;
+//   public getMembership = getMembership;
+//   public signUpAdmin = signUpAdmin;
+//   public deleteAdmin = deleteAdmin;
+//   public getUsersDatabaseUrl = getUsersDatabaseUrl;
+//   public signup = signUp;
+//   public signUp = signUp;
+//   public getUser = getUser;
+//   public putUser = putUser;
+//   public deleteUser = deleteUser;
+//   public changePassword = changePassword;
+//   public changeUsername = changeUsername;
+//   constructor() {
+//   }
+// }
+// // let plugin:any = PouchDBPlugin;
+// let plugin:PouchDBPlugin = new PouchDBPlugin();
 if (typeof window !== 'undefined' && window.PouchDB) {
     window.PouchDB.plugin(plugin);
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,22 +1,2 @@
-/// <reference types="pouchdb-core" />
-export declare class PouchDBPlugin {
-    login: (username: string, password: string, opts: PouchDB.Core.Options) => Promise<import("./utils").LoginResponse>;
-    logIn: (username: string, password: string, opts: PouchDB.Core.Options) => Promise<import("./utils").LoginResponse>;
-    logout: (opts: PouchDB.Core.Options) => Promise<PouchDB.Core.BasicResponse>;
-    logOut: (opts: PouchDB.Core.Options) => Promise<PouchDB.Core.BasicResponse>;
-    getSession: () => Promise<import("./utils").SessionResponse>;
-    getMembership: (opts: PouchDB.Core.Options) => Promise<import("./utils").CouchNodeMembership>;
-    signUpAdmin: (username: string, password: string, opts: PouchDB.Core.Options) => Promise<PouchDB.Core.BasicResponse>;
-    deleteAdmin: (username: string, opts: PouchDB.Core.Options) => Promise<PouchDB.Core.BasicResponse>;
-    getUsersDatabaseUrl: () => string;
-    signup: (username: string, password: string, opts: import("./utils").PutUserOptions) => Promise<PouchDB.Core.BasicResponse>;
-    signUp: (username: string, password: string, opts: import("./utils").PutUserOptions) => Promise<PouchDB.Core.BasicResponse>;
-    getUser: (username: string, opts: PouchDB.Core.Options) => Promise<import("./utils").PouchDBUserDoc>;
-    putUser: (username: string, opts: import("./utils").PutUserOptions) => Promise<PouchDB.Core.BasicResponse>;
-    deleteUser: (username: string, opts: PouchDB.Core.Options) => Promise<PouchDB.Core.BasicResponse>;
-    changePassword: (username: string, password: string, opts: PouchDB.Core.Options) => Promise<PouchDB.Core.BasicResponse>;
-    changeUsername: (oldUsername: string, newUsername: string, opts: import("./utils").PutUserOptions) => Promise<PouchDB.Core.BasicResponse>;
-    constructor();
-}
-declare let plugin: PouchDBPlugin;
+declare let plugin: any;
 export default plugin;
